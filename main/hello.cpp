19d4d7222926a4313d0e852b3d160023538b8bb9
Disha Deepak Shanbhag <130165423+diishanbhag@users.noreply.github.com>
2025-03-19 13:27:47 +0530

    Update hello.cpp for webhook test

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "PES2UG22CS183 has successfully built and run this  \n";
+  cout << "doing task4 lab8\n";
   return 0;
 }
